839c8ca0eeea121536cf2a6a3903fb318678a0e2
tripott <tripott@gmail.com>
2018-03-01 11:06:23 -0500

    clean up

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Portfolio from './pages/Portfolio'
 import 'tachyons'
-//import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Portfolio />, document.getElementById('root'))
-//registerServiceWorker();
